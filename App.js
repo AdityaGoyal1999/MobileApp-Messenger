ac5be9c418cf2cf41e4554af6ef6ca67351bcbd3
Aditya Goyal <adityagoyaldoon@gmail.com>
2021-09-13 18:32:37 -0400

    Setup of depedencies in ReactNative

--- App.js
+++ App.js
@@ -2,12 +2,27 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// external libraries
+import 'react-native-gesture-handler';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+// Components built 
+import LoginScreen from "./screens/LoginScreen";
+
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          title="Login Screen" 
+          name="Login" 
+          component={LoginScreen} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
